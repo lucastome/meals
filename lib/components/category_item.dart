6c15092fee4b0bcc3d96577046e97333c299d526
lucastome <flucastome@gmail.com>
2023-01-23 15:46:49 -0300

    first commit

---
+++ lib/components/category_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+    );
+  }
+}
