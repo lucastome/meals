153057b5e1605c7b655ee42cbb983f1e105851b0
lucastome <flucastome@gmail.com>
2023-01-30 09:52:33 -0300

    Meal detail screen customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
-import 'package:meals/screens/meal_detail_screent.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 import 'screens/categories_screen.dart';
 
